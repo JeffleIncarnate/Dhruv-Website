725838262aa6d917feeef11a234c48f8e97de814
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-02-04 12:42:52 +1300

    Update: We got spicy typewriter effect

--- src/components/splash/splashHome/titleHome/titleHome.tsx
+++ src/components/splash/splashHome/titleHome/titleHome.tsx
@@ -1,13 +1,16 @@
 import "./titleHome.css";
 
 import { useState, useEffect } from "react";
+import Typewriter from "typewriter-effect";
 
 export default function TitleHome() {
   return (
     <div className="DHR__TitleHome-Title">
-      <h1>
-        Hiya, I'm <span>Dhruv™️</span>!
-      </h1>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter.typeString("Hiya, I'm Dhruv™️!").start();
+        }}
+      />
       <p>I don't make websites, I make web experiences</p>
     </div>
   );
