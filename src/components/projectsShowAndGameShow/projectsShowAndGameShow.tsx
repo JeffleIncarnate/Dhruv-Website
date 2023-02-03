7344e4d46f58bdb10c28575f95e146dc1a375fc5
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-02-04 12:32:53 +1300

    Feature: Got the compnent to show games and projects

--- src/components/projectsShowAndGameShow/projectsShowAndGameShow.tsx
+++ src/components/projectsShowAndGameShow/projectsShowAndGameShow.tsx
@@ -1,10 +1,23 @@
 import "./projectsShowAndGameShow.css";
 
+import { useNavigate } from "react-router-dom";
+
 export default function ProjectsShowAndGameShow(props: any) {
+  let navigate = useNavigate();
+
   return (
-    <div>
+    <div
+      className={`projects_${props.align}`}
+      onClick={() => {
+        window.open(props.redirect, "_blank", "noreferrer");
+      }}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+          url(${props.img})`,
+      }}
+    >
       <h2>{props.number}</h2>
-      <button></button>
+      <p>{props.description}</p>
     </div>
   );
 }
