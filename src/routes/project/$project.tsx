fad1855a4a0ded3268db5d698033ae70b4f55716
Jeffle Incarnate <dhruvrayat50@gmail.com>
2025-01-04 20:41:40 +1300

    feat: filter to verify if a project is real, if it is NOT a real project, then we just redirect to home page

--- src/routes/project/$project.tsx
+++ src/routes/project/$project.tsx
@@ -1,8 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 import { ProjectHeader } from "../../components/projectHeader";
 
 import { ProjectContext } from "../../core/context/project";
+import { Projects } from "../../core/data/projects";
 
 export const Route = createFileRoute("/project/$project")({
   component: () => (
@@ -14,6 +15,24 @@ export const Route = createFileRoute("/project/$project")({
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   const { project } = Route.useParams();
+  const navigate = useNavigate();
+
+  const doesProjectExist =
+    Projects.filter((x) => {
+      return (
+        x.projectName ===
+        project
+          .split("-")
+          .map((x) => x.charAt(0).toUpperCase() + x.slice(1))
+          .join(" ")
+      );
+    }).length !== 0;
+
+  if (!doesProjectExist) {
+    navigate({
+      to: `/`,
+    });
+  }
 
   return (
     <ProjectContext.Provider value={project}>
