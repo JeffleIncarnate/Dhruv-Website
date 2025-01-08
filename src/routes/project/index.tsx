b9cd8a311202e2e2609342849e7c333581b8bc85
Jeffle Incarnate <dhruvrayat50@gmail.com>
2025-01-08 22:11:30 +1300

    feat: style quotes

--- src/routes/project/index.tsx
+++ src/routes/project/index.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/project/')({
-  component: () => <div>Hello /project/!</div>
-})
+export const Route = createFileRoute("/project/")({
+  component: () => <div>Hello /project/!</div>,
+});
